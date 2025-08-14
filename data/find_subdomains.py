8e5984689df57f41b104320a49be9ff122f57ac3
Edward <75525369+anthugeist@users.noreply.github.com>
2025-08-14 20:28:09 +0200

    support for findsubdomains

--- data/find_subdomains.py
+++ data/find_subdomains.py
@@ -9,6 +9,6 @@ def find_subdomains(domain):
     result = set()
     response = get('https://findsubdomains.com/subdomains-of/' + domain).text
     matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
-    for match in matches:
+    for match in matches: 
         result.add(match.replace(' ', '').replace('\n', ''))
     return list(result)
