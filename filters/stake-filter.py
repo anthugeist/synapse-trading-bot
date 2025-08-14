9150641ce4e74c1e4c22d11dfe22f0aef11ec888
Edward <75525369+anthugeist@users.noreply.github.com>
2025-08-14 20:24:38 +0200

    v 0.11.2

--- filters/stake-filter.py
+++ filters/stake-filter.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from typing import List, Optional, Union
  
 from socketio import Client
-from socketio.exceptions import ConnectionError as SocketIOConnectionError
+from socketio.exceptions import ConnectionError as SocketIOConnectionError 
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
