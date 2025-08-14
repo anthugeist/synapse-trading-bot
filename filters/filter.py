a24045a8a329f6919edc3424f7ebe1335bc2f546
Edward <75525369+anthugeist@users.noreply.github.com>
2025-08-14 20:25:50 +0200

    Update filter.py

--- filters/filter.py
+++ filters/filter.py
@@ -3,7 +3,7 @@ import time
 import mock
 import postgrest
 import pytest
-
+ 
 import octobot_commons.configuration as commons_configuration
 import octobot_commons.authentication as authentication
 import octobot.community as community
